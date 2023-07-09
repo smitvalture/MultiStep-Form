e9872de667f49c14e3c1846348f184502082c9be
Smit Valture <valture16@gmail.com>
2023-07-09 21:55:48 +0530

    addon plan done

--- src/pages/MultiStepForm.jsx
+++ src/pages/MultiStepForm.jsx
@@ -1,70 +1,90 @@
-import React, { useEffect, useState } from 'react'
-import sidebarImg from '../assets/images/bg-sidebar-desktop.svg'
-import Personal_Info from './steps/Personal_Info'
-import Select_Plan from './steps/Select_Plan'
-import Add_Ons from './steps/Add_Ons'
-import Finish_Up from './steps/Finish_Up'
+import React, { useEffect, useState } from 'react';
+import sidebarImg from '../assets/images/bg-sidebar-desktop.svg';
+import Personal_Info from './steps/Personal_Info';
+import Select_Plan from './steps/Select_Plan';
+import Add_Ons from './steps/Add_Ons';
+import Finish_Up from './steps/Finish_Up';
 
 const MultiStepForm = () => {
-
     const steps = [
         {
-            "step": "step 1",
-            "info": "your info",
-            "title": "Personal info",
-            "des": "Please provide your name, email address, and phone number.",
+            step: 'step 1',
+            info: 'your info',
+            title: 'Personal info',
+            des: 'Please provide your name, email address, and phone number.',
         },
         {
-            "step": "step 2",
-            "info": "select plan",
-            "title": "Select your plan",
-            "des": "You have the option of monthly or yearly billing.",
+            step: 'step 2',
+            info: 'select plan',
+            title: 'Select your plan',
+            des: 'You have the option of monthly or yearly billing.',
         },
         {
-            "step": "step 3",
-            "info": "add-ons",
-            "title": "Pick add-ons",
-            "des": "Add-ons help enhance your gaming experience.",
+            step: 'step 3',
+            info: 'add-ons',
+            title: 'Pick add-ons',
+            des: 'Add-ons help enhance your gaming experience.',
         },
         {
-            "step": "step 4",
-            "info": "summary",
-            "title": "Finishing up",
-            "des": "Double-check everything looks 0K before confirming.",
+            step: 'step 4',
+            info: 'summary',
+            title: 'Finishing up',
+            des: 'Double-check everything looks OK before confirming.',
         },
-    ]
+    ];
 
-    const [page, setPage] = useState(0)
+    const [page, setPage] = useState(0);
     const [error, setError] = useState({
-        eName: "",
-        eEmail: "",
-        ePhone: "",
-        ePlan: "",
-    })
+        eName: '',
+        eEmail: '',
+        ePhone: '',
+    });
     const [inputs, setInputs] = useState({
-        name: "",
-        email: "",
-        phone: "",
-    })
-    const [yearToggle, setYearToggle] = useState(false)
+        name: '',
+        email: '',
+        phone: '',
+    });
+    const [yearToggle, setYearToggle] = useState(false);
     const [plans, setPlans] = useState({
         arcade: 9,
         advance: 12,
         pro: 15,
         selectedPlan: 0,
-    })
-    const [activePlan, setActivePlan] = useState("")
+    });
+    const [activePlan, setActivePlan] = useState('');
 
+    const [addonPlans, setAddonPlans] = useState({
+        online: 1,
+        large: 2,
+        custome: 2,
+    });
+    const [selectedAddOns, setSelectedAddOns] = useState([]);
+    const [planCost, setPlanCost] = useState(0);
+    const [totalCost, setTotalCost] = useState(0);
 
     useEffect(() => {
-        console.log(plans.selectedPlan);
-    }, [plans.selectedPlan])
+        console.log(totalCost);
+    }, [totalCost]);
+
+    useEffect(() => {
+        if (yearToggle) {
+            setAddonPlans({
+                online: 10,
+                large: 20,
+                custome: 20,
+            });
+        } else {
+            setAddonPlans({
+                online: 1,
+                large: 2,
+                custome: 2,
+            });
+        }
+    }, [yearToggle]);
 
     function handleActivePlan(select) {
-        setActivePlan(select)
+        setActivePlan(select);
     }
-
-
 
     function handlePlans() {
         setYearToggle((curr) => !curr);
@@ -73,16 +93,29 @@ const MultiStepForm = () => {
                 arcade: 90,
                 advance: 120,
                 pro: 150,
+                selectedPlan: 0,
             });
         } else {
             setPlans({
                 arcade: 9,
                 advance: 12,
                 pro: 15,
+                selectedPlan: 0,
             });
         }
     }
+    const handleAddOnSelection = (addOn) => {
+        const isSelected = selectedAddOns.includes(addOn);
+        let updatedSelection = [];
 
+        if (isSelected) {
+            updatedSelection = selectedAddOns.filter((item) => item !== addOn);
+        } else {
+            updatedSelection = [...selectedAddOns, addOn];
+        }
+
+        setSelectedAddOns(updatedSelection);
+    };
 
 
     function handleubmit(e) {
@@ -91,143 +124,156 @@ const MultiStepForm = () => {
         let hasError = false;
 
         if (!inputs.name) {
-            setError((prevError) => ({ ...prevError, eName: "Name is required" }));
+            setError((prevError) => ({ ...prevError, eName: 'Name is required' }));
             hasError = true;
         } else {
-            setError((prevError) => ({ ...prevError, eName: "" }));
+            setError((prevError) => ({ ...prevError, eName: '' }));
         }
 
         if (!inputs.email) {
-            setError((prevError) => ({ ...prevError, eEmail: "Email is required" }));
+            setError((prevError) => ({ ...prevError, eEmail: 'Email is required' }));
             hasError = true;
         } else if (!/^\S+@\S+\.\S+$/.test(inputs.email)) {
-            setError((prevError) => ({ ...prevError, eEmail: "Enter valid email" }));
+            setError((prevError) => ({ ...prevError, eEmail: 'Enter valid email' }));
             hasError = true;
         } else {
-            setError((prevError) => ({ ...prevError, eEmail: "" }));
+            setError((prevError) => ({ ...prevError, eEmail: '' }));
         }
 
         if (!inputs.phone) {
-            setError((prevError) => ({ ...prevError, ePhone: "Phone is required" }));
+            setError((prevError) => ({ ...prevError, ePhone: 'Phone is required' }));
             hasError = true;
         } else {
-            setError((prevError) => ({ ...prevError, ePhone: "" }));
+            setError((prevError) => ({ ...prevError, ePhone: '' }));
         }
 
-
-
         if (page === 1) {
-
-            if (activePlan === "arc") {
-                setPlans({
-                    ...plans,
-                    selectedPlan: plans.arcade,
-                })
-            } else if (activePlan === "adv") {
-                setPlans({
-                    ...plans,
-                    selectedPlan: plans.advance,
-                })
-            } else if (activePlan === "pro") {
-                setPlans({
-                    ...plans,
-                    selectedPlan: plans.pro,
-                })
-            } else if (!activePlan || activePlan === "") {
-                // setError()
+            if (!activePlan || activePlan === '') {
                 hasError = true;
+            } else {
+                let selectedPlanCost = 0;
+                if (activePlan === 'arc') {
+                    selectedPlanCost = plans.arcade;
+                } else if (activePlan === 'adv') {
+                    selectedPlanCost = plans.advance;
+                } else if (activePlan === 'pro') {
+                    selectedPlanCost = plans.pro;
+                }
+                setPlanCost(selectedPlanCost)
+                setTotalCost(planCost)
             }
         }
 
-
-
-
-
-
+        if (page === 2) {
+            console.log(selectedAddOns);
+            if (selectedAddOns.length === 1 && selectedAddOns[0] === "online") {
+                setTotalCost(prev => prev + addonPlans.online);
+            } else if (selectedAddOns.length === 1 && selectedAddOns[0] === ("large" || "custome")) {
+                setTotalCost(prev => prev + addonPlans.large);
+            } else if (
+                selectedAddOns.includes("online") &&
+                selectedAddOns.includes("large" || "custome") &&
+                selectedAddOns.length === 2
+            ) {
+                setTotalCost(prev => prev + addonPlans.large + addonPlans.online);
+            } else if (
+                selectedAddOns.includes("online") &&
+                selectedAddOns.includes("large") &&
+                selectedAddOns.includes("custome") &&
+                selectedAddOns.length === 3
+            ) {
+                setTotalCost(prev => prev + (2 * addonPlans.large) + addonPlans.online);
+            }
+        }
 
         if (!hasError) {
             setPage((current) => current + 1);
         }
-
     }
 
     return (
-        <section className='w-full min-h-screen h-full flex justify-center items-center bg-[#eff4fe] font-Ubuntu'>
-            <div className='w-fit h-fit p-5 bg-white rounded-xl drop-shadow-xl flex gap-8'>
-
+        <section className="w-full min-h-screen h-full flex justify-center items-center bg-[#eff4fe] font-Ubuntu">
+            <div className="w-fit h-fit p-5 bg-white rounded-xl drop-shadow-xl flex gap-8">
                 {/* **************** SideBar ************** */}
-                <div className='relative'>
-                    <img className='-z-10' src={sidebarImg} alt="sidebarDesktop" />
-                    <div className='absolute top-0 left-0 z-10 pl-6 pt-6 text-white uppercase flex flex-col gap-5'>
-                        {
-                            steps.map((items, index) => (
-                                <div key={index} className='flex items-center gap-5'>
-                                    <p className={`w-10 h-10 pb-0.5 border duration-500 border-white ${index === page ? 'bg-[#c3e0fa] text-black' : ''} rounded-full flex justify-center items-center text-xl font-semibold`}>{index + 1}</p>
-                                    <div>
-                                        <p className='font-light opacity-75'>{items.step}</p>
-                                        <p className='font-bold'>{items.info}</p>
-                                    </div>
+                <div className="relative">
+                    <img className="-z-10" src={sidebarImg} alt="sidebarDesktop" />
+                    <div className="absolute top-0 left-0 z-10 pl-6 pt-6 text-white uppercase flex flex-col gap-5">
+                        {steps.map((items, index) => (
+                            <div key={index} className="flex items-center gap-5">
+                                <p
+                                    className={`w-10 h-10 pb-0.5 border duration-500 border-white ${index === page ? 'bg-[#c3e0fa] text-black' : ''
+                                        } rounded-full flex justify-center items-center text-xl font-semibold`}
+                                >
+                                    {index + 1}
+                                </p>
+                                <div>
+                                    <p className="font-light opacity-75">{items.step}</p>
+                                    <p className="font-bold">{items.info}</p>
                                 </div>
-                            ))
-                        }
+                            </div>
+                        ))}
                     </div>
                 </div>
-                <form onSubmit={handleubmit} noValidate className='px-10 pt-5 pb-3 w-[550px] flex flex-col justify-between'>
-
+                <form onSubmit={handleubmit} noValidate className="px-10 pt-5 pb-3 w-[550px] flex flex-col justify-between">
                     <div>
-                        <h1 className='font-bold text-3xl'>{steps[page]?.title}</h1>
-                        <p className='text-gray-400 text-sm'>{steps[page]?.des}</p>
+                        <h1 className="font-bold text-3xl">{steps[page]?.title}</h1>
+                        <p className="text-gray-400 text-sm">{steps[page]?.des}</p>
                     </div>
 
-                    {
-                        (page === 0) ?
-                            <Personal_Info
-                                eName={error.eName}
-                                eEmail={error.eEmail}
-                                ePhone={error.ePhone}
-                                valueName={inputs.name}
-                                changeName={(e) => {
-                                    setInputs((prevState) => ({
-                                        ...prevState,
-                                        name: e.target.value,
-                                    }));
-                                    setError((prevError) => ({ ...prevError, eName: "" }));
-                                }}
-
-                                valueEmail={inputs.email}
-                                changeEmail={(e) => {
-                                    setInputs((prevState) => ({
-                                        ...prevState,
-                                        email: e.target.value,
-                                    }));
-                                    setError((prevError) => ({ ...prevError, eEmail: "" }));
-                                }}
-
-                                valuePhone={inputs.phone}
-                                changePhone={(e) => {
-                                    setInputs((prevState) => ({
-                                        ...prevState,
-                                        phone: e.target.value,
-                                    }));
-                                    setError((prevError) => ({ ...prevError, ePhone: "" }));
-                                }}
-
-                            />
-
-                            : (page === 1) ? <Select_Plan
-                                onclickArc={() => handleActivePlan("arc")}
-                                onclickAdv={() => handleActivePlan("adv")}
-                                onclickPro={() => handleActivePlan("pro")}
-                                activePlan={activePlan}
-                                costArc={plans.arcade}
-                                costAdv={plans.advance}
-                                costPro={plans.pro}
-                                onclickToggle={() => handlePlans()}
-                                toggle={yearToggle}
-                            />
-                                : (page === 2) ? <Add_Ons />
-                                    : <Finish_Up />
-                    }
+                    {page === 0 ? (
+                        <Personal_Info
+                            eName={error.eName}
+                            eEmail={error.eEmail}
+                            ePhone={error.ePhone}
+                            valueName={inputs.name}
+                            changeName={(e) => {
+                                setInputs((prevState) => ({
+                                    ...prevState,
+                                    name: e.target.value,
+                                }));
+                                setError((prevError) => ({ ...prevError, eName: '' }));
+                            }}
+                            valueEmail={inputs.email}
+                            changeEmail={(e) => {
+                                setInputs((prevState) => ({
+                                    ...prevState,
+                                    email: e.target.value,
+                                }));
+                                setError((prevError) => ({ ...prevError, eEmail: '' }));
+                            }}
+                            valuePhone={inputs.phone}
+                            changePhone={(e) => {
+                                setInputs((prevState) => ({
+                                    ...prevState,
+                                    phone: e.target.value,
+                                }));
+                                setError((prevError) => ({ ...prevError, ePhone: '' }));
+                            }}
+                        />
+                    ) : page === 1 ? (
+                        <Select_Plan
+                            onclickArc={() => handleActivePlan('arc')}
+                            onclickAdv={() => handleActivePlan('adv')}
+                            onclickPro={() => handleActivePlan('pro')}
+                            activePlan={activePlan}
+                            costArc={plans.arcade}
+                            costAdv={plans.advance}
+                            costPro={plans.pro}
+                            onclickToggle={() => handlePlans()}
+                            toggle={yearToggle}
+                        />
+                    ) : page === 2 ? (
+                        <Add_Ons
+                            toggle={yearToggle}
+                            addonPlans={addonPlans}
+                            onclickArc={() => handleAddOnSelection('online')}
+                            onclickAdv={() => handleAddOnSelection('large')}
+                            onclickPro={() => handleAddOnSelection('custome')}
+                            selectedAddOns={selectedAddOns}
+                        />
+                    ) : (
+                        <Finish_Up />
+                    )}
 
                     <div></div>
                     <div></div>
@@ -235,7 +281,11 @@ const MultiStepForm = () => {
                     {/* ******************* navigation Button ******************* */}
                     <div className='flex justify-between'>
                         <div>
-                            <button onClick={() => { setPage((current) => current - 1) }} hidden={page === 0} className='text-gray-500 hover:text-black font-medium' type='button'>Go Back</button>
+                            <button onClick={() => {
+                                setPage((current) => current - 1);
+                                page == 2 && setTotalCost(0);
+                                page === 3 && setTotalCost(planCost);
+                            }} hidden={page === 0} className='text-gray-500 hover:text-black font-medium' type='button'>Go Back</button>
                         </div>
                         <div>
                             <button className={`h-8 px-4 py-0.5 text-white rounded-lg ${page === 3 ? "bg-[#4841f5] hover:bg-[#908bf7]" : "bg-[#162c57] hover:bg-[#27457f]"}`} type="submit">{page === 3 ? "Confirm" : "Next Step"}</button>
--- src/pages/steps/Add_Ons.jsx
+++ src/pages/steps/Add_Ons.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react';
+import checkIcon from '../../assets/images/icon-checkmark.svg';
 
-const Add_Ons = () => {
+const Add_Ons = ({ toggle, addonPlans, onclickArc, onclickAdv, onclickPro, selectedAddOns, handleAddOnSelection }) => {
+
+
   return (
-    <div>Add_Ons</div>
-  )
-}
+    <div className="w-full h-full flex gap-4 flex-col justify-center items-center">
+      <div
+        onClick={onclickArc}
+        className={`cursor-pointer select-none w-full h-fit flex gap-6 justify-between items-center p-5 rounded-md border-[1.8px] border-gray-300 hover:border-purple-300 ${selectedAddOns.includes('online') ? 'bg-[#f7f8ff]' : ''
+          }`}
+      >
+        <div
+          className={`w-5 h-5 p-1 flex justify-center items-center rounded border-gray-400 ${selectedAddOns.includes('online') ? 'bg-[#4840f5] border-transparent' : 'border-[1.5px]'
+            }`}
+        >
+          {selectedAddOns.includes('online') && <img src={checkIcon} alt="checkmark" />}
+        </div>
+        <div className="w-3/4 h-full">
+          <h4 className="text-lg font-semibold">Online Service</h4>
+          <p className="text-sm text-gray-400">Access to multiplayer games</p>
+        </div>
+        <p className="text-sm text-purple-600 font-semibold">
+          +${addonPlans.online}/{toggle ? 'yr' : 'mo'}
+        </p>
+      </div>
 
-export default Add_Ons
+      <div
+        onClick={onclickAdv}
+        className={`cursor-pointer select-none w-full h-fit flex gap-6 justify-between items-center p-5 rounded-md border-[1.8px] border-gray-300 hover:border-purple-300 ${selectedAddOns.includes('large') ? 'bg-[#f7f8ff]' : ''
+          }`}
+      >
+        <div
+          className={`w-5 h-5 p-1 flex justify-center items-center rounded border-gray-400 ${selectedAddOns.includes('large') ? 'bg-[#4840f5] border-transparent' : 'border-[1.5px]'
+            }`}
+        >
+          {selectedAddOns.includes('large') && <img src={checkIcon} alt="checkmark" />}
+        </div>
+        <div className="w-3/4 h-full">
+          <h4 className="text-lg font-semibold">Larger Storage</h4>
+          <p className="text-sm text-gray-400">Extra 1TB of cloud save</p>
+        </div>
+        <p className="text-sm text-purple-600 font-semibold">
+          +${addonPlans.large}/{toggle ? 'yr' : 'mo'}
+        </p>
+      </div>
+
+      <div
+        onClick={onclickPro}
+        className={`cursor-pointer select-none w-full h-fit flex gap-6 justify-between items-center p-5 rounded-md border-[1.8px] border-gray-300 hover:border-purple-300 ${selectedAddOns.includes('custome') ? 'bg-[#f7f8ff]' : ''
+          }`}
+      >
+        <div
+          className={`w-5 h-5 p-1 flex justify-center items-center rounded border-gray-400 ${selectedAddOns.includes('custome') ? 'bg-[#4840f5] border-transparent' : 'border-[1.5px]'
+            }`}
+        >
+          {selectedAddOns.includes('custome') && <img src={checkIcon} alt="checkmark" />}
+        </div>
+        <div className="w-3/4 h-full">
+          <h4 className="text-lg font-semibold">Customizable Profile</h4>
+          <p className="text-sm text-gray-400">Customize theme on your profile</p>
+        </div>
+        <p className="text-sm text-purple-600 font-semibold">
+          +${addonPlans.custome}/{toggle ? 'yr' : 'mo'}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Add_Ons;
