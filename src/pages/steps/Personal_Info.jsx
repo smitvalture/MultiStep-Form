ba226b6656b756049e5d8a4dd45c316c6f8b12a8
Smit Valture <valture16@gmail.com>
2023-07-11 01:55:32 +0530

    Responsive Done

--- src/pages/steps/Personal_Info.jsx
+++ src/pages/steps/Personal_Info.jsx
@@ -8,23 +8,23 @@ const Personal_Info = ({ eName, eEmail, ePhone, valueName, valueEmail, valuePhon
                 <div className='flex flex-col gap-1.5'>
                     <div className='flex justify-between'>
                         <h2 className='font-bold'>Name</h2>
-                        <h2 className='font-semibold text-red-500'>{eName}</h2>
+                        <p className='font-semibold text-red-500'>{eName}</p>
                     </div>
-                    <input className={`w-full h-12 border-2 border-gray-400 placeholder:font-medium ${eName ? "bg-red-100 text-red-500 border-red-500 focus:outline-red-500" : ""} rounded-lg px-4`} required value={valueName} onChange={changeName} placeholder='e.g. Stephen King' type="text" name="name" id="1" />
+                    <input className={`w-full h-12 border-2 border-gray-400 placeholder:font-medium ${eName ? "bg-red-100 text-red-500 border-red-500 focus:outline-red-500" : ""} rounded-lg px-4`} required value={valueName} onChange={changeName} placeholder='e.g. Stephen King' type="text" name="name" id={1} />
                 </div>
                 <div className='flex flex-col gap-1.5'>
                     <div className='flex justify-between'>
                         <h2 className='font-bold'>Email Address</h2>
-                        <h2 className='font-semibold text-red-500'>{eEmail}</h2>
+                        <p className='font-semibold text-red-500'>{eEmail}</p>
                     </div>
-                    <input className={`w-full h-12 border-2 border-gray-400 placeholder:font-medium ${eEmail ? "bg-red-100 text-red-500 border-red-500 focus:outline-red-500" : ""} rounded-lg px-4`} required value={valueEmail} onChange={changeEmail} placeholder='e.g. stephenking@lorem.com' type="email" name="email" id="2" />
+                    <input className={`w-full h-12 border-2 border-gray-400 placeholder:font-medium ${eEmail ? "bg-red-100 text-red-500 border-red-500 focus:outline-red-500" : ""} rounded-lg px-4`} required value={valueEmail} onChange={changeEmail} placeholder='e.g. stephenking@lorem.com' type="email" name="email" id={2} />
                 </div>
                 <div className='flex flex-col gap-1.5'>
                     <div className='flex justify-between'>
                         <h2 className='font-bold'>Phone Number</h2>
-                        <h2 className='font-semibold text-red-500'>{ePhone}</h2>
+                        <p className='font-semibold text-red-500'>{ePhone}</p>
                     </div>
-                    <input className={`w-full h-12 border-2 border-gray-400 placeholder:font-medium ${ePhone ? "bg-red-100 text-red-500 border-red-500 focus:outline-red-500" : ""} rounded-lg px-4`} required value={valuePhone} onChange={changePhone} placeholder='e.g. +91 91231 12121' type="tel" name="phone" id="3" />
+                    <input className={`w-full h-12 border-2 border-gray-400 placeholder:font-medium ${ePhone ? "bg-red-100 text-red-500 border-red-500 focus:outline-red-500" : ""} rounded-lg px-4`} required value={valuePhone} onChange={changePhone} placeholder='e.g. +91 91231 12121' type="tel" name="phone" id={3} />
                 </div>
             </div>
         </>
